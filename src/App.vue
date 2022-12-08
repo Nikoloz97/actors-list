df99b00d75be3d9df98a808c60cc703ee574e356
Nikoloz97 <nick.gotsy@gmail.com>
2022-12-07 19:57:12 -0500

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Need to hand the data off... -->
+    <hello-world v-bind:tacos="tacos"/>
   </div>
 </template>
 
@@ -10,10 +11,14 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {HelloWorld},
+  data() {
+    return {
+      tacos: ['soft', 'hard', 'crunchy', 'light', 'heavy'],
+    }
   }
 }
+
 </script>
 
 <style>
